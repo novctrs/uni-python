0f045ad292ffd0aac233494888719a4d1cf098f0
novctrs <v.menyakin@gmail.com>
2022-12-13 12:53:40 +0300

    final version with files

--- hangman.py
+++ hangman.py
@@ -12,23 +12,22 @@ word_list = {1: "light, might, white, flake, poise, noise, voice, vocal, abode, 
                 "tonight, upgrade, picture, organic, product, quality, anxious, kitchen, protein, victory, student, "
                 "mystery, liberty"}
 
-def hangman_game():
+def difficulty(level=1):
+    with open('data/hangman_results.txt', 'r') as f:
+        print(f.read())
+    words = word_list.get(level)
+    words = words.split(",")
+    return words
 
-    def difficulty():
-       level = int(input("Choose a difficulty level! type 1 for easy, 2 for medium and 3 for hard"))
-       words = word_list.get(level)
-       words=words.split(",")
-       return words
+def choose_word(words):
+    word = random.choice(words)
+    if word.startswith(" ") == True:
+        word = word.replace(" ", "")
+    letters=[]
+    letters[1:0] = word
+    return letters, word
 
-    def choose_word():
-          word = random.choice(words)
-          if word.startswith(" ") == True:
-             word = word.replace(" ", "")
-          letters=[]
-          letters[1:0] = word
-          return letters, word
-
-    def gameplay():
+def gameplay(letters, word, blank):
         tries=10
         while tries>0:
               letter = input("Guess a letter")
@@ -39,24 +38,33 @@ def hangman_game():
                  print('You guessed right!', ''.join(blank))
                  if "_" not in blank:
                     print("You've won! The word is", word)
+                    with open('data/hangman_results.txt', 'w') as f:
+                        f.write(f'last result: win, word:{word}\n')
                     break
               else:
                  tries -= 1
                  if tries == 0:
                     print(f"You've made a mistake! You've lost! The word was {word}")
+                    with open('data/hangman_results.txt', 'w') as f:
+                        f.write(f'previous result: loss, word:{word}\n')
                     break
                  else:
                     print(f"You've made a mistake! You have {tries} tries left.", ''.join(blank))
 
-    answer = input('Welcome to Hangman! If you want to play, type \'yes\'')
+def hangman_game():
+    answer = input('If you want to play, type \'yes\'')
     if answer=='yes':
-        words = difficulty()
-        letters, word = choose_word()
+        level = int(input("Choose a difficulty level! type 1 for easy, 2 for medium and 3 for hard"))
+        words = difficulty(level)
+        letters, word = choose_word(words)
         blank = "_" * len(letters)
         blank = list(blank)
-        gameplay()
+        gameplay(letters, word, blank)
         hangman_game()
     else:
         print('Thank you for playing')
 
+with open('data/hangman_results.txt','w') as f:
+    f.write("")
+print('Welcome to Hangman!')
 hangman_game()
